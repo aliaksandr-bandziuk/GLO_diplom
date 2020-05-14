fe8bfb60d187b641f389589c554e8a08b682ab54
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-05-14 23:24:56 +0300

    temp

--- src/modules/callback.js
+++ src/modules/callback.js
@@ -3,6 +3,32 @@
 // Модальное окно 1
 const callback = () => {
 
+   const callBtn = document.querySelectorAll('.call-btn'),
+      popupCall = document.querySelector('.popup-call');
+
+      let count = 0,
+         animation,
+         popupAnimation = () => {
+            animation = requestAnimationFrame(popupAnimation);
+            count++;
+            if(parseFloat(popupCall.firstElementChild.style.left) <   +parseFloat('38%')) {
+               popupCall.firstElementChild.style.left = count*3 + '%';
+         } else {
+               cancelAnimationFrame(animation);
+         }
+         };
+
+      callBtn.forEach((elem) => {
+         event.preventDefault();
+         elem.addEventListener('click', () => {
+            popupCall.style.display = 'block';
+            if(screen.width > 768) {
+               popupCall.firstElementChild.style.left = 0%;
+               animation = requestAnimationFrame(popupAnimation);
+            }
+         });
+      });
+
 };
 
 export default callback;
