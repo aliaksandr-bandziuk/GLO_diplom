8683b250a8bc6c8c86c32852636faec89b915ac2
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-05-15 15:40:16 +0300

    модальное окно 1 (функция callback)

--- src/modules/callback.js
+++ src/modules/callback.js
@@ -6,28 +6,32 @@ const callback = () => {
    const callBtn = document.querySelectorAll('.call-btn'),
       popupCall = document.querySelector('.popup-call');
 
-      let count = 0,
-         animation,
-         popupAnimation = () => {
-            animation = requestAnimationFrame(popupAnimation);
-            count++;
-            if(parseFloat(popupCall.firstElementChild.style.left) <   +parseFloat('38%')) {
-               popupCall.firstElementChild.style.left = count*3 + '%';
-         } else {
-               cancelAnimationFrame(animation);
-         }
-         };
-
+         
       callBtn.forEach((elem) => {
-         event.preventDefault();
+         elem.addEventListener('click', function(event){
+            event.preventDefault();
+         });
+
          elem.addEventListener('click', () => {
             popupCall.style.display = 'block';
-            if(screen.width > 768) {
-               popupCall.firstElementChild.style.left = 0%;
-               animation = requestAnimationFrame(popupAnimation);
-            }
          });
       });
+
+      popupCall.addEventListener('click', (event) => {
+         let target = event.target;
+ 
+         if(target.classList.contains('popup-close')){
+            popupCall.style.display = 'none';
+         } else {
+            target = target.closest('.popup-content');
+ 
+            if(!target){
+               popupCall.style.display = 'none';
+            }
+         }
+      });
+
+
 
 };
 
