3ec5d32288a17413a1e2c9b15e01ff89ea71b2f7
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-05-15 22:00:20 +0300

    popup-check

--- src/modules/getCheckList.js
+++ src/modules/getCheckList.js
@@ -3,6 +3,33 @@
 // Скидка (получить чек-лист за скидку)
 const getCheckList = () => {
 
+   const checkBtn = document.querySelectorAll('.check-btn'),
+      popupCheck = document.querySelector('.popup-check');
+
+         
+      checkBtn.forEach((elem) => {
+         elem.addEventListener('click', function(event){
+            event.preventDefault();
+         });
+
+         elem.addEventListener('click', () => {
+            popupCheck.style.display = 'block';
+         });
+      });
+
+      popupCheck.addEventListener('click', (event) => {
+         let target = event.target;
+ 
+         if(target.classList.contains('popup-close')){
+            popupCheck.style.display = 'none';
+         } else {
+            target = target.closest('.popup-content');
+ 
+            if(!target){
+               popupCheck.style.display = 'none';
+            }
+         }
+      });
 };
 
 export default getCheckList;
