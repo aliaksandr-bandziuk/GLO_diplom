022b1d046528b148063cdbb34cb42fbe32037205
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-05-17 16:00:08 +0300

    Кнопка Больше

--- src/modules/getMore.js
+++ src/modules/getMore.js
@@ -3,6 +3,25 @@
 // Кнопка Больше
 const getMore = () => {
 
+   const sentenceRow = document.querySelector('.sentence .row'),
+      addSentenceBtn = document.querySelector('.add-sentence-btn');
+      
+      addSentenceBtn.addEventListener('click', () => {
+         let a = !0,
+            b = !1,
+            c = void 0;
+         try {
+            for (let i, d = sentenceRow.children[Symbol.iterator](); !(a = (i = d.next()).done); a = !0) {
+               let m = i.value;
+               m.classList.contains('visible-sm-block') 
+               && m.classList.remove('visible-sm-block'), m.classList.contains('hidden') 
+               && m.classList.remove('hidden'), addSentenceBtn.style.display = 'none';
+            }
+         } catch (sentenceRow) {
+            b = !0, c = sentenceRow;
+         }
+      });
+
 };
 
 export default getMore;
