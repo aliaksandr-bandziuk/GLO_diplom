80f4a387ab79a5d40d61b87d24d962f9a2370670
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-05-20 16:19:38 +0300

    sended

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 import "@babel/polyfill";
-import 'nodelist-foreach-polyfill';
+import "nodelist-foreach-polyfill";
 import elementClosest from "element-closest";
 elementClosest(window);
 import "formdata-polyfill";
@@ -9,39 +9,40 @@ import "es6-promise";
 import "fetch-polyfill";
 
 (function (arr) {
-   arr.forEach(function (item) {
-     if (item.hasOwnProperty('append')) {
-       return;
-     }
-     Object.defineProperty(item, 'append', {
-       configurable: true,
-       enumerable: true,
-       writable: true,
-       value: function append() {
-         var argArr = Array.prototype.slice.call(arguments),
-           docFrag = document.createDocumentFragment();
-         
-         argArr.forEach(function (argItem) {
-           var isNode = argItem instanceof Node;
-           docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
-         });
-         
-         this.appendChild(docFrag);
-       }
-     });
-   });
- })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+ arr.forEach(function (item) {
+  if (item.hasOwnProperty("append")) {
+   return;
+  }
+  Object.defineProperty(item, "append", {
+   configurable: true,
+   enumerable: true,
+   writable: true,
+   value: function append() {
+    var argArr = Array.prototype.slice.call(arguments),
+     docFrag = document.createDocumentFragment();
 
+    argArr.forEach(function (argItem) {
+     var isNode = argItem instanceof Node;
+     docFrag.appendChild(
+      isNode ? argItem : document.createTextNode(String(argItem))
+     );
+    });
 
-import callback from './modules/callback';
-import getConsult from './modules/getConsult';
-import getDiscount from './modules/getDiscount';
-import accordion from './modules/accordion';
-import getCheckList from './modules/getCheckList';
-import calc from './modules/calc';
-import getMore from './modules/getMore';
-import sendForm from './modules/sendForm';
-import formValidation from './modules/formValidation';
+    this.appendChild(docFrag);
+   },
+  });
+ });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+import callback from "./modules/callback";
+import getConsult from "./modules/getConsult";
+import getDiscount from "./modules/getDiscount";
+import accordionFunc from "./modules/accordionFunc";
+import getCheckList from "./modules/getCheckList";
+import calc from "./modules/calc";
+import getMore from "./modules/getMore";
+import sendForm from "./modules/sendForm";
+import formValidation from "./modules/formValidation";
 
 // Модальное окно 1
 callback();
@@ -50,7 +51,7 @@ getConsult();
 // Модальное окно 2 — Заказать со скидкой
 getDiscount();
 // Аккордеон
-accordion();
+accordionFunc();
 // Скидка (получить чек-лист за скидку)
 getCheckList();
 // Калькулятор-аккордеон
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,77 +1,76 @@
-'use strict';
+"use strict";
 
 const calc = () => {
-   const onoffswitchCheckbox = document.querySelector('.onoffswitch-checkbox'),
-   formControl = document.querySelectorAll('.form-control'),
-   panelBody = document.getElementById('collapseTwo'),
-   contentPanelBody = panelBody.children[0].children,
-   accordion = document.querySelector('#accordion'),
-   calcResult = document.querySelector('#calc-result'),
-   myonoffswitchTwo = document.querySelector('#myonoffswitch-two'),
-   distanceInput = document.querySelector('#collapseFour').children[0].children[1],
-   distanceBtn = document.querySelector('#collapseFour').children[0].children[2];
-   let maintotal = 0;
+ const onoffswitchCheckbox = document.querySelector(".onoffswitch-checkbox"),
+  formControl = document.querySelectorAll(".form-control"),
+  panelBody = document.getElementById("collapseTwo"),
+  contentPanelBody = panelBody.children[0].children,
+  accordion = document.querySelector("#accordion"),
+  calcResult = document.querySelector("#calc-result"),
+  myonoffswitchTwo = document.querySelector("#myonoffswitch-two"),
+  distanceInput = document.querySelector("#collapseFour").children[0]
+   .children[1],
+  distanceBtn = document.querySelector("#collapseFour").children[0].children[2];
+ let maintotal = 0;
 
-   accordion.addEventListener('click', () => {
-       if (onoffswitchCheckbox.checked){
-           contentPanelBody[5].classList.add('hidden');
-           contentPanelBody[4].classList.add('hidden');
-           contentPanelBody[3].classList.add('hidden');
-           
-           let total = 10000;
-           maintotal = total;
-           if (contentPanelBody[1].children[1].value === '2 метра'){
-               maintotal = total + (total * 0.2);
-           }  
-           if (contentPanelBody[2].children[1].value === '2 штуки'){
-               maintotal += (total * 0.3);
-           }   else if (contentPanelBody[2].children[1].value === '3 штуки'){
-               maintotal += (total * 0.5);
-           }
-           if (myonoffswitchTwo.checked){
-               maintotal += 1000;
-           }
-       }   else{
-           contentPanelBody[5].classList.remove('hidden');
-           contentPanelBody[4].classList.remove('hidden');
-           contentPanelBody[3].classList.remove('hidden');
-           let total = 15000;
-           maintotal = total;
-           if (contentPanelBody[1].children[1].value === '2 метра'){
-               maintotal += (total * 0.2);
-           }  
-           if (contentPanelBody[2].children[1].value === '2 штуки'){
-               maintotal += (total * 0.3);
-           }   else if (contentPanelBody[2].children[1].value === '3 штуки'){
-               maintotal += (total * 0.5);
-           }
-           if (contentPanelBody[4].children[1].value === '2 метра'){
-               maintotal += (total * 0.2);
-           }  
-           if (contentPanelBody[5].children[1].value === '2 штуки'){
-               maintotal += (total * 0.3);
-           }   else if (contentPanelBody[5].children[1].value === '3 штуки'){
-               maintotal += (total * 0.5);
-           }
-           if (myonoffswitchTwo.checked){
-               maintotal += 2000;
-           }
+ accordion.addEventListener("click", () => {
+  if (onoffswitchCheckbox.checked) {
+   contentPanelBody[5].classList.add("hidden");
+   contentPanelBody[4].classList.add("hidden");
+   contentPanelBody[3].classList.add("hidden");
 
-       }
-       calcResult.value = maintotal;
-       if (distanceInput.value === ''){
-           distanceBtn.disabled = true;
-       }
-       distanceInput.addEventListener('input', () => {
-           distanceInput.value = distanceInput.value.replace (/[^0-9]/, '');
-           if (distanceInput.value === ''){
-               distanceBtn.disabled = true;
-           }   else{
-               distanceBtn.disabled = false;
-           }
-       });
-       
-   });  
+   let total = 10000;
+   maintotal = total;
+   if (contentPanelBody[1].children[1].value === "2 метра") {
+    maintotal = total + total * 0.2;
+   }
+   if (contentPanelBody[2].children[1].value === "2 штуки") {
+    maintotal += total * 0.3;
+   } else if (contentPanelBody[2].children[1].value === "3 штуки") {
+    maintotal += total * 0.5;
+   }
+   if (myonoffswitchTwo.checked) {
+    maintotal += 1000;
+   }
+  } else {
+   contentPanelBody[5].classList.remove("hidden");
+   contentPanelBody[4].classList.remove("hidden");
+   contentPanelBody[3].classList.remove("hidden");
+   let total = 15000;
+   maintotal = total;
+   if (contentPanelBody[1].children[1].value === "2 метра") {
+    maintotal += total * 0.2;
+   }
+   if (contentPanelBody[2].children[1].value === "2 штуки") {
+    maintotal += total * 0.3;
+   } else if (contentPanelBody[2].children[1].value === "3 штуки") {
+    maintotal += total * 0.5;
+   }
+   if (contentPanelBody[4].children[1].value === "2 метра") {
+    maintotal += total * 0.2;
+   }
+   if (contentPanelBody[5].children[1].value === "2 штуки") {
+    maintotal += total * 0.3;
+   } else if (contentPanelBody[5].children[1].value === "3 штуки") {
+    maintotal += total * 0.5;
+   }
+   if (myonoffswitchTwo.checked) {
+    maintotal += 2000;
+   }
+  }
+  calcResult.value = maintotal;
+  if (distanceInput.value === "") {
+   distanceBtn.disabled = true;
+  }
+  distanceInput.addEventListener("input", () => {
+   distanceInput.value = distanceInput.value.replace(/[^0-9]/, "");
+   if (distanceInput.value === "") {
+    distanceBtn.disabled = true;
+   } else {
+    distanceBtn.disabled = false;
+   }
+  });
+ });
 };
 
 export default calc;
